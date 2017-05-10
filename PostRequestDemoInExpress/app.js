4e71dbb278eeec3d6df5526315c7bdb0e3678a1d
Christos Constantinides <constantinides.ch@gmail.com>
2017-05-10 12:55:24 +0200

    Post Request Demo In Express
    Using body-parser, express & ejs

---
+++ PostRequestDemoInExpress/app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+var friends = ["Kalio", "Cosi", "Adam", "Patrick", "Bambo", "Mike", "Marino"];
+
+app.get("/", function(req, res){
+   res.render("home"); 
+});
+
+app.get("/friends", function(req, res) {
+    res.render("friends", {friends: friends});
+});
+
+app.post("/addFriend", function(req, res){
+    var newFriend = req.body.newfriend;
+    friends.push(newFriend);
+    res.redirect("/friends");
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function (){
+    console.log("starting server...")
+});
